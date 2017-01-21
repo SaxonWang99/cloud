9d3226a4430bdb2ae6f66c0bcc9b10245bce4a8a
Paul Nguyen <paul.nguyen@sjsu.edu>
2017-01-21 00:46:20 -0800

    Updated Spring Boot JPA Examples

---
+++ spring/hellojpa_v2/src/sql/ddl.sql
@@ -0,0 +1,11 @@
+
+CREATE SCHEMA IF NOT EXISTS `test` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `test`;
+
+CREATE TABLE IF NOT EXISTS `test`.`student` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(128) NOT NULL,
+  `last_name` VARCHAR(128) NOT NULL,
+  `age` INT NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
