08ef0bc3f5510ffec21f2f420a6fe8922ef2af30
Paul Nguyen <paul.nguyen@sjsu.edu>
2017-09-10 22:00:16 -0700

    Fixed docker.sh login

--- grails/hellorest/docker.sh
+++ grails/hellorest/docker.sh
@@ -35,6 +35,8 @@ docker_auth () {
    $PROMPT "Docker Password: \c" ; read -s pass ; 
    echo "" ;
    docker login -u $user -p $pass
+   #docker login -u $user -p $pass
+   #TMP=`cat ~/.docker/config.json | grep  \"auth\": | wc -l | sed -e 's/^[ \t]*//'`
    TMP=`docker login -u $user -p $pass | grep Succeeded | wc -l | sed -e 's/^[ \t]*//'`
    #echo ".${TMP}."
    if [ "$TMP" == "1" ] ; 
--- restlet/gumball/docker.sh
+++ restlet/gumball/docker.sh
@@ -34,11 +34,12 @@ docker_auth () {
    $PROMPT "Docker Userid:   \c" ; read user ;
    $PROMPT "Docker Password: \c" ; read -s pass ; 
    echo "" ;
-   docker login -u $user -p $pass
-   TMP=`cat ~/.docker/config.json | grep  \"auth\": | wc -l | sed -e 's/^[ \t]*//'`
+    #docker login -u $user -p $pass
+   #TMP=`cat ~/.docker/config.json | grep  \"auth\": | wc -l | sed -e 's/^[ \t]*//'`
+   TMP=`docker login -u $user -p $pass | grep Succeeded | wc -l | sed -e 's/^[ \t]*//'`
    #echo ".${TMP}."
    if [ "$TMP" == "1" ] ; 
-   then 
+  then 
    		AUTH="TRUE" ; 
    		ACCOUNT=$user ;
    else 
